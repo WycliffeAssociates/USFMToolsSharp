7dfa7299ffd07823096b65f3e086fd4ae60e4b30
BryanHo10 <bryankho1010@gmail.com>
2019-06-10 14:51:32 -0400

    renders footnotes

--- USFMToolsSharp/HtmlRenderer.cs
+++ USFMToolsSharp/HtmlRenderer.cs
@@ -8,20 +8,29 @@ namespace USFMToolsSharp
     public class HtmlRenderer
     {
         public List<string> UnrenderableTags;
+        public List<string> FootnoteTextTags;
+
         private bool isSingleSpaced = true;
         private bool hasOneColumn = true;
         private bool isL2RDirection = true;
-        private bool isTextJustified = true;
+        private bool isTextJustified = false;
+        private bool separateChapters = false;
         public HtmlRenderer()
         {
             UnrenderableTags = new List<string>();
+            FootnoteTextTags = new List<string>();
         }
-        public HtmlRenderer(bool isSingleSpaced, bool hasOneColumn, bool isL2RDirection, bool isTextJustified)
+        public HtmlRenderer(bool isSingleSpaced, bool hasOneColumn, bool isL2RDirection, bool isTextJustified, bool separateChapters)
         {
             this.isSingleSpaced = isSingleSpaced;
             this.hasOneColumn = hasOneColumn;
             this.isL2RDirection = isL2RDirection;
             this.isTextJustified = isTextJustified;
+            this.separateChapters = separateChapters;
+
+            UnrenderableTags = new List<string>();
+            FootnoteTextTags = new List<string>();
+
         }
 
         public string Render(USFMDocument input)
@@ -41,13 +50,15 @@ namespace USFMToolsSharp
             // HTML tags can only have one class, when render to docx
             output.AppendLine($"<body class=\"{(isSingleSpaced ? "" : "double-space")}\">");
             output.AppendLine($"<div class=\"{ (hasOneColumn ? "" : "multi-column")}\">");
-            output.AppendLine($"<div class=\"{ (isTextJustified ? "" : "justified")}\"> ");
+            output.AppendLine($"<div class=\"{ (isTextJustified ? "justified" : "")}\"> ");
             output.AppendLine($"<div class=\"{ (isL2RDirection ? "" : "rtl-direct")}\"> ");
 
             foreach (Marker marker in input.Contents)
             {
                 output.Append(RenderMarker(marker));
             }
+
+            output.AppendLine(RenderFootnotes());
 
             output.AppendLine("</div>");
             output.AppendLine("</div>");
@@ -89,7 +100,9 @@ namespace USFMToolsSharp
                         output.Append(RenderMarker(marker));
                     }
                     output.AppendLine("</div>");
-                    output.AppendLine("<br class=\"pagebreak\"></br>");
+
+                    // Page breaks after each chapter
+                    output.AppendLine($"{(separateChapters?"<br class=\"pagebreak\"></br>" : "")}");
                     break;
                 case VMarker vMarker:
                     output.AppendLine($"<span class=\"verse\">");
@@ -137,12 +150,57 @@ namespace USFMToolsSharp
                     output.AppendLine(mTMarker.Title);
                     output.AppendLine("</div>");
                     break;
+                case FMarker fMarker:
+                    foreach (Marker marker in input.Contents)
+                    {
+                        RenderMarker(marker);
+                    }
+                    break;
+                case FTMarker fTMarker:
+                    string footnote="";
+                    foreach(Marker marker in input.Contents)
+                    {
+                        switch (marker)
+                        {
+                            case TextBlock textBlock:
+                                footnote += textBlock.Text+" ";
+                                break;
+                            case FQAMarker fQAMarker:
+                                footnote += "<span class=\"footnote-quote\">";
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                    FootnoteTextTags.Add(footnote);
+                    break;
+                case FQAEndMarker fQAEndMarker:
+                    FootnoteTextTags[FootnoteTextTags.Count-1] += "</span>" + " ";
+                    break;
+
+                //case FootnoteTextBlock footnoteTextBlock:
+                //    FootnoteTextTags.Add(footnoteTextBlock.Text);
+                //    break;
                 case IDEMarker _:
                 case IDMarker _:
                     break;
                 default:
                     UnrenderableTags.Add(input.Identifier);
                     break;
+            }
+            return output.ToString();
+        }
+        private string RenderFootnotes()
+        {
+            StringBuilder output = new StringBuilder();
+            output.AppendLine("<br class=\"pagebreak\"></br>");
+            output.AppendLine("<div class=\"header\">Footnotes</div>");
+            foreach (string footnote in FootnoteTextTags)
+            {
+                output.AppendLine("<div>");
+                output.Append(footnote);
+                output.AppendLine("</div>");
             }
             return output.ToString();
         }
