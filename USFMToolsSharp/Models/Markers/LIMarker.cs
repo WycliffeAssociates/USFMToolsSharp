07ea2a83f8931f1424111fcc547973e35b927e4a
Bryan Ho <bryankho1010@gmail.com>
2019-07-05 13:55:37 -0400

    added renderable list markers (#24)

---
+++ USFMToolsSharp/Models/Markers/LIMarker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    /// <summary>
+    /// List Entry Marker
+    /// </summary>
+    public class LIMarker : Marker
+    {
+        public int Depth = 1;
+        public string Text;
+        public override string Identifier => "li";
+        public override string PreProcess(string input)
+        {
+            return string.Empty;
+        }
+        public override List<Type> AllowedContents
+        {
+            get
+            {
+                return new List<Type>()
+                {
+                    typeof(TextBlock)
+                };
+            }
+        }
+    }
+}
