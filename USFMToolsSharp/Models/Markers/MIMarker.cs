1003e2029d433be9903eb68076566fd591575379
Bryan Ho <bryankho1010@gmail.com>
2019-07-05 17:13:59 -0400

    Indent flush left paragraph (#30)

---
+++ USFMToolsSharp/Models/Markers/MIMarker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    /// <summary>
+    /// Indented flush left paragraph
+    /// </summary>
+    public class MIMarker : Marker
+    {
+        public override string Identifier => "mi";
+    }
+}
