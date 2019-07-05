962691cc6b3547eec7ed864bec4abd046c460a90
BryanHo10 <bryankho1010@gmail.com>
2019-07-05 16:30:13 -0400

    Added small cap text support

---
+++ USFMToolsSharp/Models/Markers/SCMarker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    /// <summary>
+    /// Small-Cap Letter 
+    /// </summary>
+    public class SCMarker : Marker
+    {
+        public override string Identifier => "sc";
+        public override List<Type> AllowedContents => new List<Type>() {
+            typeof(TextBlock),
+        };
+    }
+}
