b47ab903c63788eea0279a04892e231e06f22a34
Bryan Ho <bryankho1010@gmail.com>
2019-07-08 10:02:14 -0400

    Added support for Major section headings and references (#28)

---
+++ USFMToolsSharp/Models/Markers/MRMarker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    /// <summary>
+    /// Major section reference marker
+    /// </summary>
+    public class MRMarker : Marker
+    {
+        public int Weight = 1;
+        public string SectionReference;
+        public override string Identifier => "mr";
+        public override string PreProcess(string input)
+        {
+            SectionReference= input;
+            return string.Empty;
+        }
+    }
+}
