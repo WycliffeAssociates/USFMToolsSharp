56cfd3c0567aeb22fff954f3867f1963d0ddafa0
danparisd <43418833+danparisd@users.noreply.github.com>
2019-07-10 11:33:00 -0400

    fix path to the package
    Sorry for committing directly to master, but I just realized this mistake and it's a direct fix.

--- build_and_nuget_push.ps1
+++ build_and_nuget_push.ps1
@@ -1,4 +1,4 @@
 dotnet build -c Release
 cd .\USFMToolsSharp\bin\Release
-$PkgName = Get-ChildItem -Name -Path .\USFMToolsSharp\bin\release\*.nupkg
+$PkgName = Get-ChildItem -Name -Path .\*.nupkg
 dotnet nuget push $PkgName -k $NUGET_TOKEN -s https://api.nuget.org/v3/index.json
