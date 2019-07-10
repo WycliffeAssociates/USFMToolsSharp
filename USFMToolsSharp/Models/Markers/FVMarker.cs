48a573cdd0286300d4a8feebf3253e3d7f33e693
BryanHo10 <bryankho1010@gmail.com>
2019-07-10 11:21:12 -0400

    Add support for FV Markers

---
+++ USFMToolsSharp/Models/Markers/FVMarker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    public class FVMarker : Marker
+    {
+        public string VerseCharacter;
+        public override string Identifier => "fv";
+        public override string PreProcess(string input)
+        {
+            VerseCharacter = input;
+            return string.Empty;
+        }
+    }
+}
