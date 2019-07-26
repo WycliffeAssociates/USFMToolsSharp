01426948f78a0064923e8349ae868ffd04a88fac
BryanHo10 <bryankho1010@gmail.com>
2019-07-26 13:39:03 -0400

    Added support for character style markers

---
+++ USFMToolsSharp/Models/Markers/NOMarker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    /// <summary>
+    /// Normal Text
+    /// </summary>
+    public class NOMarker : Marker
+    {
+        public override string Identifier => "no";
+        public override List<Type> AllowedContents => new List<Type>() {
+            typeof(TextBlock)
+        };
+    }
+}
---
+++ USFMToolsSharp/Models/Markers/SUPEndMarker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace USFMToolsSharp.Models.Markers
+{
+    public class SUPEndMarker : Marker
+    {
+        public override string Identifier => "sup*";
+    }
+}
