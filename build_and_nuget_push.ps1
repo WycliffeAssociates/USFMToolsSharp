221afa033b60925e30f0e5457b03e6af85ebcf4f
danparisd <43418833+danparisd@users.noreply.github.com>
2019-08-08 17:12:21 -0400

    Delete build_and_nuget_push.ps1
    don't need this if we're building in travis

--- build_and_nuget_push.ps1
+++
@@ -1,4 +0,0 @@
-dotnet build -c Release
-cd .\USFMToolsSharp\bin\Release
-$PkgName = Get-ChildItem -Name -Path .\*.nupkg
-dotnet nuget push $PkgName -k $Env:NUGET_TOKEN -s https://api.nuget.org/v3/index.json
