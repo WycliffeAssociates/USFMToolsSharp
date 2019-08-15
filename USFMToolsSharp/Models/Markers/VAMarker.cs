85f3b51fa59bb073f0ae0adeeb21eec0ddbd1187
Bryan Ho <bryankho1010@gmail.com>
2019-08-15 10:07:07 -0400

    Update Verse/Chapter Alternate Markers (#108)
    * CA Markers accept only integer as AltChapterNumber
    
    * Removed VP child markers from VA Markers

--- USFMToolsSharp/Models/Markers/VAMarker.cs
+++ USFMToolsSharp/Models/Markers/VAMarker.cs
@@ -10,31 +10,12 @@ namespace USFMToolsSharp.Models.Markers
     public class VAMarker : Marker
     {
         public string AltVerseNumber;
-        public string AltVerseCharacter
-        {
-            get
-            {
-                var firstCharacterMarker = GetChildMarkers<VPMarker>();
-                if (firstCharacterMarker.Count > 0)
-                {
-                    return firstCharacterMarker[0].VerseCharacter;
-                }
-                else
-                {
-                    return AltVerseNumber;
-                }
-            }
-        }
+        
         public override string Identifier => "va";
         public override string PreProcess(string input)
         {
             AltVerseNumber = input;
             return string.Empty;
         }
-        public override List<Type> AllowedContents => new List<Type>()
-        {
-            typeof(VPMarker),
-            typeof(VPEndMarker)
-        };
     }
 }
