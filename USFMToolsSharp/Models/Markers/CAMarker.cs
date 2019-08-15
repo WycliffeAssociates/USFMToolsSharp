afc326510fd286a845046ab87cb0f9311a16bb71
Bryan Ho <hob@wycliffeassociates.org>
2019-08-15 09:49:19 -0400

    CA Markers accept only integer as AltChapterNumber

--- USFMToolsSharp/Models/Markers/CAMarker.cs
+++ USFMToolsSharp/Models/Markers/CAMarker.cs
@@ -10,30 +10,12 @@ namespace USFMToolsSharp.Models.Markers
     public class CAMarker : Marker
     {
         public string AltChapterNumber;
-        public string AltChapterCharacter
-        {
-            get
-            {
-                var firstCharacterMarker = GetChildMarkers<CPMarker>();
-                if (firstCharacterMarker.Count > 0)
-                {
-                    return firstCharacterMarker[0].PublishedChapterMarker;
-                }
-                else
-                {
-                    return AltChapterCharacter;
-                }
-            }
-        }
+
         public override string Identifier => "ca";
         public override string PreProcess(string input)
         {
             AltChapterNumber = input;
             return string.Empty;
         }
-        public override List<Type> AllowedContents => new List<Type>()
-        {
-            typeof(CPMarker),
-        };
     }
 }
