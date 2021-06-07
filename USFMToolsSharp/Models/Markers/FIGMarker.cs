93c9a8b1bff0cfabc958c4d1f59a27faa4e0224c
Robertson Brinker <robertsonbrinker@gmail.com>
2021-06-07 12:03:46 -0400

    \fig v3 functionality and unit tests

--- USFMToolsSharp/Models/Markers/FIGMarker.cs
+++ USFMToolsSharp/Models/Markers/FIGMarker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace USFMToolsSharp.Models.Markers
@@ -26,35 +27,74 @@ namespace USFMToolsSharp.Models.Markers
             string[] wordEntry = input.Split('|');
             if (wordEntry.Length > 0)
             {
-                Caption = wordEntry[0];
+                Description = wordEntry[0].Trim();
             }
             if (wordEntry.Length > 1)
             {
-                Description = wordEntry[1];
+                FilePath = wordEntry[1].Trim();
             }
             if (wordEntry.Length > 2)
             {
-                Width = wordEntry[2];
+                Width = wordEntry[2].Trim();
             }
             if (wordEntry.Length > 3)
             {
-                Location = wordEntry[3];
+                Location = wordEntry[3].Trim();
             }
             if (wordEntry.Length > 4)
             {
-                Copyright = wordEntry[4];
+                Copyright = wordEntry[4].Trim();
             }
             if (wordEntry.Length > 5)
             {
-                Reference = wordEntry[5];
+               Caption = wordEntry[5].Trim();
             }
             if (wordEntry.Length > 6)
             {
-                FilePath = wordEntry[6];
+                Reference = wordEntry[6].Trim();
             }
 
 
+            
+            string[] contentArr = input.Split('|');
+            if (contentArr.Length > 0 && contentArr.Length <= 2)
+            {
+                Caption = contentArr[0].Trim();
+
+                string[] attributes = contentArr[1].Split('"');
+                for (int i = 0; i < attributes.Length; i++)
+                {
+                    if (attributes[i].Replace(" ", "").Contains("alt="))
+                    {
+                        Description = attributes[i + 1].Trim();
+                    }
+                    if (attributes[i].Replace(" ", "").Contains("src="))
+                    {
+                        FilePath = attributes[i + 1].Trim();
+                    }
+                    if (attributes[i].Replace(" ", "").Contains("size="))
+                    {
+                        Width = attributes[i + 1].Trim();
+                    }
+                    if (attributes[i].Replace(" ", "").Contains("loc="))
+                    {
+                        Location = attributes[i + 1].Trim();
+                    }
+                    if (attributes[i].Replace(" ", "").Contains("copy="))
+                    {
+                        Copyright = attributes[i + 1].Trim();
+                    }
+                    if (attributes[i].Replace(" ", "").Contains("ref="))
+                    {
+                        Reference = attributes[i + 1].Trim();
+                    }
+                }
+            }
+
+            
+
             return string.Empty;
+            
         }
 
     }
