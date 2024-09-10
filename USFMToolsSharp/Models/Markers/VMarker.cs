766d5b520891e5cd75df21ccc629a58a57bb5d23
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2024-09-10 16:30:16 -0400

    Fixes for id_ayt

--- USFMToolsSharp/Models/Markers/VMarker.cs
+++ USFMToolsSharp/Models/Markers/VMarker.cs
@@ -139,6 +139,7 @@ namespace USFMToolsSharp.Models.Markers
                     typeof(REMMarker),
                     typeof(PMarker),
                     typeof(LIMarker),
+                    typeof(PCMarker),
                 };
         public override bool TryInsert(Marker input)
         {
